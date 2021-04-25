aecbdf49857a0171116e7b7d9ec917cc800c7292
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-04-25 13:36:11 -0300

    functions are now proper documented. pycaret.regression now import as alias, not as wildcard

--- airbnb_prediction/modelling.py
+++ airbnb_prediction/modelling.py
@@ -1,23 +1,33 @@
-import shap
-import pickle
-from pycaret.regression import *
-from airbnb_prediction import objects
+import pandas as pd
+import pycaret.regression as pcr
+from airbnb_prediction import config
 
 
 class RegressorTrainer:
     def __init__(self, df: pd.DataFrame, target: str, exp_name: str, session_id: int = 16):
+        """
+        Initialize classe objects.
+        :param df: Cleaned dataframe for model ingestion.
+        :param target: Target variable.
+        :param exp_name: Model experiment name, for mlflow tracking purposes.
+        :param session_id: experiment's random state.
+        """
         self.df = df
         self.target = target
         self.exp_name = exp_name
         self.session_id = session_id
 
     def start_session(self):
-        setup(
+        """
+        Starts pycaret environment.
+        :return: None
+        """
+        pcr.setup(
             data=self.df,
             target=self.target,
             experiment_name=self.exp_name,
-            categorical_features=objects.pycaret_categorical_features,
-            numeric_features=objects.pycaret_numerical_features,
+            categorical_features=config.pycaret_categorical_features,
+            numeric_features=config.pycaret_numerical_features,
             session_id=self.session_id,
             normalize=True,
             silent=True,
@@ -25,21 +35,49 @@ class RegressorTrainer:
         )
 
     def train_model(self):
+        """
+        Train and optimize model.
+        :return: Final model.
+        """
         print('Training LightGBM: Step 1/3')
-        lightgbm = create_model('lightgbm')
+        lightgbm = pcr.create_model('lightgbm',
+                                verbose=False)
+
         print('Training Tuned LightGBM, Optimize = RMSE: Step 2/3')
-        tuned_lightgbm = tune_model(lightgbm, optimize='RMSE')
+        tuned_lightgbm = pcr.tune_model(lightgbm, optimize='RMSE',
+                                    verbose=False)
+
         print('Training Ensemble LightGBM: Step 3/3')
-        self.model = ensemble_model(tuned_lightgbm)
+        self.model = pcr.ensemble_model(tuned_lightgbm,
+                                    verbose=False)
+        self.metrics = pcr.pull()
 
     def finalize_model(self):
-        finalize_model(self.model)
+        """
+        This function fits the estimator onto the complete
+        dataset passed during the setup() stage.
+        The purpose of this function is to
+        prepare for final model deployment after experimentation.
+        Description taken from pycaret/classification.
+        :return: Trained model object fitted on complete dataset.
+        """
+        pcr.finalize_model(self.model)
 
     def save_model(self, path: str):
-        pickle.dump(self.model, open(path, 'wb'))
+        """
+        Saves model to path.
+        :param path: Path to save model.
+        :return: None
+        """
+        pcr.save_model(self.model, path)
 
     def predict_model(self, data: pd.DataFrame):
-        prediction = predict_model(estimator=self.model, data=data)
+        """
+        Predicts model on unseen data.
+        :param data: a new unseen dataframe.
+        :return: float. Prediction on unseen data.
+        """
+        prediction = pcr.predict_model(estimator=self.model, data=data)
         return prediction['Label'][0]
 
     @property
