dac6d1c31c7b21e652ef0a3f4f63f8dd0b9c2bf6
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-04-25 20:38:00 -0300

    created main.py

---
+++ airbnb_prediction/main.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from airbnb_prediction import preprocess, config, modelling
+import pickle
+
+
+def features():
+    """
+    Method to split, clean and preprocess data.
+    :return:
+    """
+    # Splits
+    df_train, df_test = preprocess.spliting_dataset()
+
+    preprocess.preprocess_data(df_train)
+    preprocess.preprocess_data(df_test)
+
+    pickle.dump(df_train, open('../data/processed/data_train.pickle', 'wb'))
+    pickle.dump(df_test, open('../data/processed/data_test.pickle', 'wb'))
+
+
+def deploy_model():
+
+    df = pickle.load(open('../data/processed/data_train.pickle', 'rb'))
+
+    # Model Stage
+    print('Starting Model Stage')
+    model = modelling.RegressorTrainer(df.drop('id', axis=1), 'price', "testing")
+
+    print('Setting up Pycaret Environment')
+    model.start_session()
+
+    print("Training The Model")
+    model.train_model()
+
+    print("Finalizing The Model")
+    model.finalize_model()
+
+    print('Generated Model Saved at: {}'.format(config.model_path))
+    model.save_model("{}/model".format(config.model_path))
+
+
+def evaluate_model():
+    df = pickle.load(open('../data/processed/data_test.pickle', 'rb'))
+
+    model = modelling.RegressorTrainer(df.drop('id', axis=1), 'price', "validation")
+    model.load_model("{}/model".format(config.model_path))
+
+    prediction = model.predict_model(df, export_metrics=True)
+    pickle.dump(prediction, open('../data/processed/prediction.pickle', 'wb'))
+
+
+def run():
+    features()
+    deploy_model()
+    evaluate_model()
+
+
+def cli():
+    """ Caller to transform module in a low-level CLI """
+    return
+
+
+if __name__ == '__main__':
+    cli()
