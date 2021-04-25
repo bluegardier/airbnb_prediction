bb61122c57166de6a5b93d0090e30075a3f7e601
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-04-25 15:24:42 -0300

    Now calls preprocess_data function to handle the preprocess step

--- airbnb_prediction/train.py
+++ airbnb_prediction/train.py
@@ -4,22 +4,10 @@ from airbnb_prediction import preprocess, config, modelling
 import pickle
 
 if __name__ == '__main__':
+    # Preprocess Step
     df = pd.read_csv('{}/listings_train.csv'.format(config.data_dir_raw))
+
     preprocess.preprocess_data(df)
-
-    df = preprocess.dropping_empty_columns(df)
-
-    fillna_dict = {
-        'host_response_time': 'no_info',
-        'host_is_superhost': df['host_is_superhost'].mode()[0],
-        'bedrooms': df['bedrooms'].mode()[0],
-        'beds': df['beds'].mode()[0],
-        'days_since_host': df['days_since_host'].mode()[0]
-    }
-
-    df.fillna(fillna_dict, inplace=True)
-
-    df.drop(config.to_drop, axis=1, inplace=True)
     pickle.dump(df, open('../data/processed/model_data.pickle', 'wb'))
 
     # Model Stage
@@ -40,7 +28,3 @@ if __name__ == '__main__':
 
     print('Model Performance:')
     print(round(model.metrics.loc['Mean'], 2))
-
-
-
-
