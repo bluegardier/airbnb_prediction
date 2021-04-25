494a6ad3be9f0dce077fb2b5fce1e77a3ac87164
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-04-25 14:10:17 -0300

    preprocess_data now runs from preprocess.py

--- airbnb_prediction/train.py
+++ airbnb_prediction/train.py
@@ -3,51 +3,9 @@ import numpy as np
 from airbnb_prediction import preprocess, config, modelling
 import pickle
 
-
-def preprocess_data(dataframe: pd.DataFrame) -> pd.DataFrame:
-    """
-    Final Configuration do generate dataframe model.
-    :param dataframe:
-    :return:
-    """
-    dataframe['price'] = dataframe['price'] \
-        .apply(lambda x: preprocess.convert_price_to_int(x))
-
-    dataframe['days_since_host'] = \
-        (pd.to_datetime('today') - pd.to_datetime(dataframe['host_since'])).dt.days
-
-    dataframe['bathroom_text_clean'] = \
-        preprocess.extract_numbers(dataframe, 'bathrooms_text', fillna=True)
-
-    dataframe['bathrooms'] = np.where(dataframe['bathroom_text_clean'].isnull() == False,
-                                      (dataframe['bathroom_text_clean']).astype(float).apply(np.floor), 0)
-
-    dataframe['half_bath'] = \
-        np.where(dataframe['bathroom_text_clean'].str.isalnum() == False, 'yes', 'no')
-
-    dataframe['delta_nights'] = \
-        preprocess.creating_delta_variable(dataframe, 'minimum_nights', 'maximum_nights')
-
-    dataframe['delta_date_reviews'] = \
-        preprocess.creating_delta_date_variable(dataframe, 'first_review', 'last_review')
-
-    dataframe['mean_reviews'] = \
-        dataframe['number_of_reviews'] / (dataframe['number_of_reviews'].fillna(0) + 1)
-
-    dataframe['regiao'] = preprocess.creating_zones(dataframe)
-
-    dataframe['property_type_refactor'] = \
-        preprocess.creating_property_type_refactor(dataframe)
-
-    dataframe['is_host_rj'] = preprocess.creating_host_location(df)
-
-    preprocess.count_characters_variables(dataframe, config.string_variables)
-    return dataframe
-
-
 if __name__ == '__main__':
     df = pd.read_csv('{}/listings_train.csv'.format(config.data_dir_raw))
-    preprocess_data(df)
+    preprocess.preprocess_data(df)
 
     df = preprocess.dropping_empty_columns(df)
 
